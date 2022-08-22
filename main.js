37fe7c65702a123de850a99a491899227e38b2f2
vinothCuteUI <55774471+vinothCuteUI@users.noreply.github.com>
2022-08-22 11:17:09 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 const counters = document.querySelectorAll('.count');
-    const speed = 200;
+const speed = 200;
 
-    counters.forEach((counter) => {
-      const updateCount = () => {
-        const target = parseInt(counter.getAttribute('data-target'));
-        const count = parseInt(counter.innerText);
-        const increment = Math.trunc(target / speed);
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = parseInt(counter.getAttribute('data-target'));
+    const count = parseInt(counter.innerText);
+    const increment = Math.trunc(target / speed);
 
-        if (count < target) {
-          counter.innerText = count + increment;
-          setTimeout(updateCount, 1);
-        } else {
-          counter.innerText = target;
-        }
-      };
-      updateCount();
-    });
+    if (count < target) {
+      counter.innerText = count + increment;
+      setTimeout(updateCount, 1);
+    } else {
+      counter.innerText = target;
+    }
+  };
+  updateCount();
+});
